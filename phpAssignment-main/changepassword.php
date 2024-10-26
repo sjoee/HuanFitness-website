43ea70dc627fd0237e8a0fa9dce7e262481b7b9a
joee <143257256+sjoee@users.noreply.github.com>
2024-10-26 16:13:16 +0800

    removed the big title

--- phpAssignment-main/changepassword.php
+++ phpAssignment-main/changepassword.php
@@ -94,13 +94,13 @@ return true;
 	                                                                              
 	<!-- Page top Section -->
 	<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
-		<div class="container">
+<!-- 		<div class="container">
 			<div class="row">
 				<div class="col-lg-7 m-auto text-white">
 					<h2>changepassword</h2>
 				</div>
 			</div>
-		</div>
+		</div> -->
 	</section>
 
 
