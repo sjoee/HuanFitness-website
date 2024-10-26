573e2cf81104355dd64fc13590caa6f23d46f819
joee <143257256+sjoee@users.noreply.github.com>
2024-10-26 16:12:33 +0800

    removed the big title

--- phpAssignment-main/about.php
+++ phpAssignment-main/about.php
@@ -55,13 +55,13 @@ echo "<script>window.location.href='booking-history.php'</script>";
 	                                                                              
 	<!-- Page top Section -->
 	<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
-		<div class="container">
+<!-- 		<div class="container">
 			<div class="row">
 				<div class="col-lg-7 m-auto text-white">
 					<h2>About Us</h2>
 				</div>
 			</div>
-		</div>
+		</div> -->
 	</section>
 
 
