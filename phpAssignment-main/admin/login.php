db6fc2878133270ee56b918e20077b0c9f775f1d
Tterrence05 <142971374+Tterrence05@users.noreply.github.com>
2024-10-29 11:25:49 +0800

    Update login.php

--- phpAssignment-main/admin/login.php
+++ phpAssignment-main/admin/login.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 error_reporting(0);
@@ -17,10 +16,9 @@ if(isset($_POST['submit'])) {
       $count = $stmt->rowCount();
       $row   = $stmt->fetch(PDO::FETCH_ASSOC);
       if($count == 1 && !empty($row)) {
-        /******************** Your code ***********************/
-        $_SESSION['adminid']   = $row['id'];
+        $_SESSION['adminid'] = $row['id'];
         $_SESSION['email'] = $row['email'];
-        $_SESSION['name'] = $row['fname'];
+        $_SESSION['name'] = $row['name'];
        header("location: index.php");
       } else {
         $msg = "Invalid username and password!";
@@ -39,9 +37,7 @@ if(isset($_POST['submit'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Main CSS-->
     <link rel="stylesheet" type="text/css" href="css/main.css">
-    <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>GYM MS | Admin login</title>
   </head>
@@ -56,56 +52,35 @@ if(isset($_POST['submit'])) {
       <div class="login-box">
         <form class="login-form" method="post">
           <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>SIGN IN</h3>
-           <?php if($error){?><div class="errorWrap" style="color:red;"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-                else if($msg){?><div class="succWrap" style="color:red;"><strong>Error</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+           <?php if($msg){?><div class="succWrap" style="color:red;"><strong>Error</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
           <div class="form-group">
-            <label class="control-label">Emai </label>
+            <label class="control-label">Email</label>
             <input class="form-control" name="email" id="email" type="text" placeholder="Email" autofocus>
           </div>
           <div class="form-group">
-            <label class="control-label">PASSWORD</label>
+            <label class="control-label">Password</label>
             <input class="form-control" name="password" id="password" type="password" placeholder="Password">
           </div>
           <div class="form-group">
-            <div class="utility">
-      
-  <!--             <p class="semibold-text mb-2"><a href="#" data-toggle="flip">Forgot Password ?</a></p> -->
-            </div>
+            <input type="checkbox" id="togglePassword">
+            <label for="togglePassword">Show Password</label>
+          </div>
+          <div class="form-group">
+            <a href="forgot_password.php" class="forgot-password-link">Forgot Password?</a>
           </div>
           <div class="form-group btn-container">
-           
             <input type="submit" name="submit" id="submit" value="SIGN IN" class="btn btn-primary btn-block">
           </div>
           <hr />
           <a href="../index.php">Back to Home Page</a>
         </form>
-        <form class="forget-form" action="index.html">
-          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-lock"></i>Forgot Password ?</h3>
-          <div class="form-group">
-            <label class="control-label">EMAIL</label>
-            <input class="form-control" type="text" placeholder="Email">
-          </div>
-          <div class="form-group btn-container">
-            <button class="btn btn-primary btn-block"><i class="fa fa-unlock fa-lg fa-fw"></i>RESET</button>
-          </div>
-          <div class="form-group mt-3">
-            <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="fa fa-angle-left fa-fw"></i> Back to Login</a></p>
-          </div>
-        </form>
       </div>
     </section>
-    <!-- Essential javascripts for application to work-->
     <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="js/plugins/pace.min.js"></script>
     <script type="text/javascript">
-      // Login Page Flipbox control
-      $('.login-content [data-toggle="flip"]').click(function() {
-      	$('.login-box').toggleClass('flipped');
-      	return false;
+      document.getElementById("togglePassword").addEventListener("change", function() {
+        var passwordField = document.getElementById("password");
+        passwordField.type = this.checked ? "text" : "password";
       });
     </script>
   </body>
