3507b2165848c764dfebb5505669e690ff30bb95
joee <143257256+sjoee@users.noreply.github.com>
2024-10-31 17:45:43 +0800

    final

--- phpAssignment-main/contact.php
+++ phpAssignment-main/contact.php
@@ -48,8 +48,6 @@ if(isset($_POST['submit_user_form'])) {
 <head>
     <title>Gym Management System</title>
     <meta charset="UTF-8">
-    <!-- <meta name="description" content="Ahana Yoga HTML Template">
-    <meta name="keywords" content="yoga, html"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Stylesheets -->
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
