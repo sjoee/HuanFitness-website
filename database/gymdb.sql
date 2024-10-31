94dc5d6fe106cd474e5141768d6fe3d890762a7c
joee <143257256+sjoee@users.noreply.github.com>
2024-10-31 17:55:01 +0800

    Update gymdb.sql

--- database/gymdb.sql
+++ database/gymdb.sql
@@ -273,7 +273,29 @@ INSERT INTO `water_consumption` (`id`, `amount`, `date`, `time`) VALUES
 (9, 550, '2024-01-13', '15:30:00'),
 (10, 1000, '2024-01-14', '18:00:00');
 
+CREATE TABLE IF NOT EXISTS water_management (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    uid INT,
+    Cdate DATE,
+    water_consumed DECIMAL(5,2),
+    FOREIGN KEY (uid) REFERENCES tbluser(id)
+);
 
+CREATE TABLE IF NOT EXISTS weight_management (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    uid INT,
+    Wdate DATE,
+    weightKG DECIMAL(5,2),
+    FOREIGN KEY (uid) REFERENCES tbluser(id)
+);
+
+CREATE TABLE IF NOT EXISTS exercise_management (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    uid INT,
+    Edate DATE,
+    exercise_type VARCHAR(255),
+    FOREIGN KEY (uid) REFERENCES tbluser(id)
+);
 
 --
 -- Indexes for dumped tables
