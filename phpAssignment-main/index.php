7ecec5f5c2ff2b6faf8f48c18c2d4578713055fc
joee <143257256+sjoee@users.noreply.github.com>
2024-10-31 17:46:14 +0800

    final

--- phpAssignment-main/index.php
+++ phpAssignment-main/index.php
@@ -22,8 +22,6 @@ if (isset($_POST['submit'])) {
 <head>
     <title>HuanFitnessPal - Your Health Companion</title>
     <meta charset="UTF-8">
-    <meta name="description" content="Ahana Yoga HTML Template">
-    <meta name="keywords" content="yoga, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Stylesheets -->
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
